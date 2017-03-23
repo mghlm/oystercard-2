22e62ba09becabc9b35ce315a854303521b22bbe
Magnus Holm <mail@magnusholm.com>
2017-03-23 16:39:03 +0000

    create start method in journelyLog class

--- lib/journey_log.rb
+++ lib/journey_log.rb
@@ -1,8 +1,16 @@
+require_relative 'journey'
+
 class JourneyLog
 
-  attr_reader :journey_class
+  attr_reader :journey_class, :journey_history
 
   def initialize(journey_class)
     @journey_class = journey_class
+    @journey_history = []
   end
+
+  def start(entry_station)
+  end
+
+
 end
--- spec/journey_log_spec.rb
+++ spec/journey_log_spec.rb
@@ -3,10 +3,23 @@ require 'journey_log'
 describe JourneyLog do
   subject(:journey_log) {described_class.new(fake_new_journey)}
   let(:fake_new_journey) {double(:journey_class)}
+  let(:entry_station) {double(:entry_station)}
+  let(:exit_station) {double(:exit_station)}
 
   describe "#initialize" do
     it 'should initialize with one argument' do
       expect(journey_log.journey_class).to eq fake_new_journey
     end
+    it 'should create an empty array' do
+      expect(journey_log.journey_history).to eq []
+    end
   end
+
+  describe "#start" do
+    it { is_expected.to respond_to(:start).with(1).argument }
+    end
+
+    it 'should start a journey with an entry station' do
+    expect(journey_log.start(entry_station)).to eq entry_station
+    end
 end
