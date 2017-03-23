b2723a04e269666beff144d4132de5bf5ed8703d
Magnus Holm <mail@magnusholm.com>
2017-03-23 14:18:11 +0000

    created fare method and set penalty- and minimum fare. Made tests pass

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,6 +1,8 @@
 class Journey
 
   attr_reader :entry_station, :exit_station
+  PENALTY_FARE = 6
+  MINIMUM_FARE = 1
 
   def start(entry_station)
     @entry_station = entry_station
@@ -11,10 +13,17 @@ class Journey
   end
 
   def fare
+    if exit_station.nil?
+      PENALTY_FARE
+    elsif entry_station.nil?
+      PENALTY_FARE
+    else
+      MINIMUM_FARE
+    end
   end
 
-  def finish
-
+  def complete?
+    !exit_station.nil?
   end
 
 
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -5,9 +5,15 @@ describe Journey do
   let(:entry_station) {double(:entry_station)}
   let(:exit_station) {double(:exit_station)}
 
+  it { is_expected.to respond_to :complete? }
+
 describe "#start" do
   it 'starts a journey' do
     expect(journey.entry_station).to eq nil
+  end
+  it "indicates if a journey is complete" do
+  journey.start(entry_station)
+  expect(journey.complete?).to be_falsy
   end
 end
 
@@ -15,8 +21,27 @@ describe "#end" do
   it "records exit station" do
     expect(journey.end(exit_station)).to eq exit_station
   end
+  it "indicates if a journey is complete" do
+    journey.start(entry_station)
+    journey.end(exit_station)
+    expect(journey.complete?).to be_truthy
+  end
+ end
+
+ describe '#fare' do
+   it 'charges a penalty fare if no entry but exit' do
+     journey.end(exit_station)
+     expect(journey.fare).to eq Journey::PENALTY_FARE
+   end
+   it 'charges a penalty fare if entry but no exit' do
+     journey.start(entry_station)
+     expect(journey.fare).to eq Journey::PENALTY_FARE
+  end
+  it 'charges the minimum fare for a complete journey' do
+    journey.start(entry_station)
+    journey.end(exit_station)
+    expect(journey.fare).to eq Journey::MINIMUM_FARE
+  end
 end
-
-
 
 end
